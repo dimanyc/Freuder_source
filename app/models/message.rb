971ca7355102338fd3220a1c954116eb7b5083ab
dimanyc <dimitrynazarov@gmail.com>
2015-01-14 14:54:02 -0500

    changing the layout, adding more columns to saved messages to show avatars and metions

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ActiveRecord::Base
 	# Active Relations: 
-	belongs_to :messageable, polymorphic: true 
+	belongs_to :messageable, polymorphic: true
+	has_many :filters
 
 	# Validations:
 	validates :body, length: {minimum: 3, maximum: 140}, allow_blank: false, uniqueness: true
@@ -10,10 +11,11 @@ class Message < ActiveRecord::Base
 
 	def self.refresh_tweets
 		tweets = $client.home_timeline
+		#tweets =
 
 		if tweets
 			tweets.each do |tweet|
-				Message.create( body:tweet.text, author:tweet.user.screen_name)
+				Message.create( body: tweet.text, author:$client.status(tweet).user.screen_name, author_image_url: $client.status(tweet).user.profile_image_url_https)
 			end
 		end	# @tweets.each do |tweet|
 	end
