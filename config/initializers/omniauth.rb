cfdcccd9f3b674ba6fb59b2c52f228d3bff24cb0
dimanyc <dimitrynazarov@gmail.com>
2015-02-16 18:11:27 -0500

    adding env with dot_env rails

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "gsrp95ZrYl09Twsu6CixoCz6G", "0ME90XGPeFK1cdrTdrRUpqhYtCG6SkdKnjH9bL8u9Vtu7pDgTy"
+  provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
     {
 		:secure_image_url => 'true',
 		:image_size => 'original',
